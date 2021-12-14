2c877d78ffb87ef1fa7b1fa415600d5cddf5b9b9
mustonentimmo <62522763+mustonentimmo@users.noreply.github.com>
2021-12-14 15:36:36 +0200

    Remove unnecessary components

--- src/components/NavItem/NavItem.js
+++
@@ -1,9 +0,0 @@
-import './NavItem.scss'
-
-const navItem = (props) => (
-        <li className="main-nav__nav-item">
-            <a href="#" className="main-nav__nav-item__link">{props.title}</a>
-        </li>
-)
-
-export default navItem
--- src/components/NavMenu/NavMenu.js
+++
@@ -1,9 +0,0 @@
-import './NavMenu.scss'
-
-const NavMenu = (props) => (
-        <ul className={`main-nav__menu ${props.open ? 'main-nav__menu--is-active' : ''}`}>
-            {props.children}
-        </ul>
-)
-
-export default NavMenu
