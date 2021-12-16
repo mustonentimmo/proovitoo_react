045c5810782cdf9c01ed11055ec3027272d18652
TimmoMustonen <mustonentimmo@gmail.com>
2021-12-16 11:28:44 +0200

    Add main container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from "../Header/Header";
+import BlogPost from "../BlogPost/BlogPost";
 
 const App = () => (
     <div className="App">
       <Header />
+        <main className="App__inner">
+            <BlogPost />
+        </main>
     </div>
 )
 
