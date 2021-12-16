fd2aa8fbe6ec9ae9ee14b8ea8f8647e6cd2cef10
TimmoMustonen <mustonentimmo@gmail.com>
2021-12-16 13:02:54 +0200

    Add BlogPost HTML structure

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,7 +1,23 @@
 import "./BlogPost.scss"
 
 const BlogPost = () => (
-    <div></div>
+    <div className="blog-post">
+        <img className="blog-post__img" src="#" alt="" />
+        <div className="blog-post__details">
+            <div className="blog-post__title"></div>
+            <div className="blog-post__meta">
+                Published by <span className="blog-post__author"></span> on <span className="blog-post__published-date"></span>
+            </div>
+            <div className="blog-post__text">
+            </div>
+            <div className="blog-post__tags">
+                <div className="blog-post__tag"></div>
+            </div>
+            <ul className="blog-post__feedbacks">
+                <li className="blog-post__feedback"></li>
+            </ul>
+        </div>
+    </div>
 )
 
 export default BlogPost
