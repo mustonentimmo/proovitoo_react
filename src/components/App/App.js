1f1989be1103f7ff36914aaac6979b80879ea85f
TimmoMustonen <mustonentimmo@gmail.com>
2021-12-16 16:45:34 +0200

    Add grid container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import './App.scss';
 import Header from "../Header/Header";
 import BlogPost from "../BlogPost/BlogPost";
 
 const App = () => (
     <div className="App">
-      <Header />
-        <main className="App__inner">
-            <BlogPost />
-        </main>
+        <div className="App__grid-container">
+            <Header />
+            <main className="App__inner">
+                <BlogPost />
+            </main>
+        </div>
     </div>
 )
 
