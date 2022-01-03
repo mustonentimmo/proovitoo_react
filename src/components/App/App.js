bc9bfa517e1b144f618371fcfc8857324c647b72
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-03 10:44:29 +0200

    Add map function to BlogPost component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,18 @@
 import './App.scss';
 import Header from "../Header/Header";
 import BlogPost from "../BlogPost/BlogPost";
+import posts from "../../mock/post";
 
 const App = () => (
     <div className="app">
         <div className="app__grid-container">
             <Header />
             <main className="app__inner">
-                <BlogPost />
+                {
+                    posts.map(post =>
+                        <BlogPost key={post.id} post={post}/>
+                    )
+                }
             </main>
         </div>
     </div>
