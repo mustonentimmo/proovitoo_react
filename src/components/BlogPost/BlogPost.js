b89349238adafc8da5945fef5c1cd15e4b71db97
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-03 10:53:34 +0200

    Remove posts array and map function, Add post props

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,55 +1,30 @@
 import "./BlogPost.scss"
 import HorizontalList from "../HorizontalList/HorizontalList";
 
-const posts = [
-    {
-        id: 1,
-        title: "Practical Functional Programming",
-        postImg: "https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1478&q=80",
-        author: "Timmo Mustonen",
-        publishedAt: "16.12.2021",
-        postText: "The following is reformatted from a presentation I gave at LinkedIn last year. The presentation attempted to explain functional programming without using concepts like “monads” or “immutability” or “side effects”. Instead it focuses on how thinking about composition can make you a better programmer, regardless of what language you use.",
-        postTags: ["#learning", "#react"],
-        postFeedback: ["5 views", "3 comments", "2 likes"]
-    },
-    {
-        id: 2,
-        title: "Writing CSS with Accessibility in Mind",
-        postImg: "https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1478&q=80",
-        author: "Timmo Mustonen",
-        publishedAt: "16.12.2021",
-        postText: "The following is reformatted from a presentation I gave at LinkedIn last year. The presentation attempted to explain functional programming without using concepts like “monads” or “immutability” or “side effects”. Instead it focuses on how thinking about composition can make you a better programmer, regardless of what language you use.",
-        postTags: ["#CSS"],
-        postFeedback: ["5 views", "3 comments", "2 likes"]
-    }
-]
-
-const BlogPost = () => (
-    posts.map(post =>(
-        <div key={post.id} className="blog-post">
-            <img className="blog-post__img" src={post.postImg} alt="" />
+const BlogPost = (props) => (
+        <div className="blog-post">
+            <img className="blog-post__img" src={props.post.postImg} alt="" />
             <div className="blog-post__details">
                 <div className="blog-post__title">
-                    <h1>{post.title}</h1>
+                    <h1>{props.post.title}</h1>
                 </div>
                 <div className="blog-post__meta">
-                    Published by <span className="blog-post__author">{post.author}</span> on <span className="blog-post__published-date">{post.publishedAt}</span>
+                    Published by <span className="blog-post__author">{props.post.author}</span> on <span className="blog-post__published-date">{props.post.publishedAt}</span>
                 </div>
                 <div className="blog-post__text">
-                    <p>{post.postText}</p>
+                    <p>{props.post.postText}</p>
                 </div>
                 <div className="blog-post__tags">
                 {
-                    post.postTags.map(tag => (
+                    props.post.postTags.map(tag => (
                         <a key={tag} href="#" className="blog-post__tag">{tag}</a>
                         )
                     )
                 }
                 </div>
-                <HorizontalList data={post.postFeedback}/>
+                <HorizontalList data={props.post.postFeedback}/>
             </div>
         </div>
-    ))
 )
 
 export default BlogPost
