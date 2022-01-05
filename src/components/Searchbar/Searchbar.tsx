5645a02445f64e565902484970542dbe9700af90
mustonentimmo <62522763+mustonentimmo@users.noreply.github.com>
2022-01-05 11:22:53 +0200

    Add component HTML structure

---
+++ src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,10 @@
+import "./Searchbar.scss"
+
+const Searchbar = () => (
+    <div className="searchbar">
+        <input className="searchbar__input" type="text"/>
+        <div className="searchbar__icon"></div>
+    </div>
+)
+
+export default Searchbar
