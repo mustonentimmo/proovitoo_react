79dbdf175938dfedae4e78fd08fa62fcc0cc919f
mustonentimmo <62522763+mustonentimmo@users.noreply.github.com>
2022-01-05 13:12:31 +0200

    Add Searchbar component to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import Header from "../Header/Header";
+import Searchbar from "../Searchbar/Searchbar";
 import BlogPost from "../BlogPost/BlogPost";
 import posts from "../../mock/post";
 
@@ -8,6 +9,7 @@ const App = () => (
         <div className="app__grid-container">
             <Header />
             <main className="app__inner">
+            <Searchbar />
                 {
                     posts.map(post =>
                         <BlogPost key={post.id} post={post}/>
