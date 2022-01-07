b98c4c86a4070f662120fcbbd5caeab289b16353
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-07 15:50:10 +0200

    Add avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Avatar.scss"
+
+interface AvatarProps {
+    imgUrl: string
+}
+
+const Avatar: React.FC<AvatarProps> = ({ imgUrl }) => (
+    <div className="avatar">
+        <img src={imgUrl} alt=""/>
+    </div>
+)
+
+export default Avatar
