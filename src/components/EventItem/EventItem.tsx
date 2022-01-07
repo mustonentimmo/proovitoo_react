9b1fe0031d643d538c2e64bd1cbef1c87c4fb77a
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-07 12:13:51 +0200

    Refactor component name

---
+++ src/components/EventItem/EventItem.tsx
@@ -0,0 +1,9 @@
+import "./EventItem.scss";
+
+const EventItem = () => (
+    <div className="event-item">
+        <div className="event-item__info"></div>
+    </div>
+)
+
+export default EventItem
