cd993dd6c2f7f94f65a8e15a4adef3e06468edc4
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-07 11:36:50 +0200

    Add HTML element for sidebar section title

--- src/components/SidebarSection/SidebarSection.tsx
+++ src/components/SidebarSection/SidebarSection.tsx
@@ -7,7 +7,11 @@ interface SidebarSectionProps {
 
 const SidebarSection: React.FC<SidebarSectionProps> = (props) => (
     <section className="sidebar-section">
-        <div className="sidebar-section__header">{props.header}</div>
+        <div className="sidebar-section__header">
+            <div className="sidebar-section__title">
+                {props.header}
+            </div>
+        </div>
         <div className="sidebar-section__content">{props.children}</div>
     </section>
 )
