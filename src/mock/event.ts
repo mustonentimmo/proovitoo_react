04b370514b42dabf9335fd28dc8933eb1c6714fb
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-07 13:15:28 +0200

    Add event mocking data

---
+++ src/mock/event.ts
@@ -0,0 +1,55 @@
+import {Event} from "../common/types";
+
+export const event: Event = {
+    events: [
+        {
+            name: "UX Tartu",
+            date: "11 Dets",
+            data: ["SPARK", "Tartu", "Tomorrow"]
+        },
+        {
+            name: "Christmas party",
+            date: "11 Dets",
+            data: ["SPARK", "Tartu", "Going"]
+        },
+        {
+            name: "Refresh 2021",
+            date: "11 Dets",
+            data: ["SPARK", "Tartu"]
+        },
+    ],
+    birthdays: [
+        {
+            name: "Steven Heidel",
+            profilePicture: "https://mustonentimmo.github.io/Iglu_proovitoo/images/Steven_Heidel.png",
+            data: ["Nov 12", "Tomorrow"]
+        },
+        {
+            name: "Eva solomon",
+            profilePicture: "https://mustonentimmo.github.io/Iglu_proovitoo/images/Eva_Solomon.png",
+            data: ["Nov 10", "Going"]
+        },
+        {
+            name: "Sarah Goldstein",
+            profilePicture: "https://mustonentimmo.github.io/Iglu_proovitoo/images/Sarah_Goldstein.png",
+            data: ["Nov 29"]
+        },
+    ],
+    newComers: [
+        {
+            name: "Steven Heidel",
+            profilePicture: "https://mustonentimmo.github.io/Iglu_proovitoo/images/Steven_Heidel.png",
+            data: ["Nov 11"]
+        },
+        {
+            name: "Eva solomon",
+            profilePicture: "https://mustonentimmo.github.io/Iglu_proovitoo/images/Eva_Solomon.png",
+            data: ["Nov 10"]
+        },
+        {
+            name: "Sarah Goldstein",
+            profilePicture: "https://mustonentimmo.github.io/Iglu_proovitoo/images/Sarah_Goldstein.png",
+            data: ["Sept 1"]
+        },
+    ]
+}
