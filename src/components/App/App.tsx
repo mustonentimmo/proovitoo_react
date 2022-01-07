8db08119e925dae18f18c93b6b4a8729b06cc1a4
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-07 15:52:28 +0200

    Add avatar, event item component and event mock data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,9 +4,13 @@ import Header from "../Header/Header";
 import Searchbar from "../Searchbar/Searchbar";
 import Sidebar from "../Sidebar/Sidebar";
 import SidebarSection from "../SidebarSection/SidebarSection";
+import Avatar from "../Avatar/Avatar";
+import EventItem from "../EventItem/EventItem";
 import BlogPost from "../BlogPost/BlogPost";
-import { Post } from "../../common/types"
+import { Post, EventType } from "../../common/types"
 import posts from "../../mock/post";
+import event from "../../mock/event"
+
 
 const App = () => {
     let [filteredPosts, setFilteredPosts] = useState<Post[]>(posts);
@@ -37,10 +41,8 @@ const App = () => {
 
                 </SidebarSection>
                 <SidebarSection title={"birthdays"}>
-
                 </SidebarSection>
                 <SidebarSection title={"newcomers"}>
-
                 </SidebarSection>
             </Sidebar>
         </div>
