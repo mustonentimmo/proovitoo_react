e3fbf63b09dad65c5394d9c4416b5c04abec06d1
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-07 09:33:28 +0200

    Add sidebar section component HTML structure

---
+++ src/components/SidebarSection/SidebarSection.tsx
@@ -0,0 +1,10 @@
+import "./SidebarSection.scss";
+
+const SidebarSection = () => (
+    <section className="sidebar-section">
+        <div className="sidebar-section__header"></div>
+        <div className="sidebar-section__content"></div>
+    </section>
+)
+
+export default SidebarSection
