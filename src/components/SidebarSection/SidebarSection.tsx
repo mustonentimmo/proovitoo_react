07003b09f44760c35c305554b2e06e83f74f6c2f
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-07 11:46:52 +0200

    Refactor interface type data name

--- src/components/SidebarSection/SidebarSection.tsx
+++ src/components/SidebarSection/SidebarSection.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./SidebarSection.scss";
 
 interface SidebarSectionProps {
-    header: string;
+    title: string;
 }
 
 const SidebarSection: React.FC<SidebarSectionProps> = (props) => (
     <section className="sidebar-section">
         <div className="sidebar-section__header">
             <h1 className="sidebar-section__title">
-                {props.header}
+                {props.title}
             </h1>
         </div>
         <div className="sidebar-section__content">{props.children}</div>
