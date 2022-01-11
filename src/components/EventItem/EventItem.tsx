f889e4616f3f50fcbab64227058237e46ef84cf5
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-11 12:08:42 +0200

    Refactor eventItem component

--- src/components/EventItem/EventItem.tsx
+++ src/components/EventItem/EventItem.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import "./EventItem.scss";
 import HorizontalList from "../HorizontalList/HorizontalList";
+import Avatar from "../Avatar/Avatar";
+import Date from "../Date/Date";
 
 interface EventItemProps {
     title: string;
     eventData: string[];
+    isAvatar?: boolean;
+    avatar?: string;
+    eventDate?: string;
 }
 
 const EventItem: React.FC<EventItemProps>= (props) => {
 
-    const { title, children, eventData } = props;
+    const { title, eventData, isAvatar, eventDate, avatar } = props;
 
     return (
     <div className="event-item">
-        {children}
+        {isAvatar ? <Avatar imgUrl={avatar} /> : <Date info={eventDate} />}
         <div className="event-item__info">
             <h3 className="event-item__title">{title}</h3>
             <HorizontalList feedback={eventData} className={"horizontal-list--small-margin-top"}/>
