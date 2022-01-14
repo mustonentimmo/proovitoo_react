a0a4c6ce6c48adc4d84df84880ffa8fddb3972e3
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-14 10:49:00 +0200

    Add dynamically changing border color to date component

--- src/components/Date/Date.tsx
+++ src/components/Date/Date.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import "./Date.scss";
 
 interface DateProps {
-    info?: string;
+    date?: string;
 }
 
-const Date: React.FC<DateProps> = ({ info }) => (
-    <div className="date">{info}</div>
-)
+const Date: React.FC<DateProps> = ({ date }) => {
+
+    const colors = ["#fcd3d4", "#a0f4dc", "#f9d371", "#93ffd8"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    const addLinebreak = (str?: string) => {
+        return str ? str.replace(" ", "\n") : "";
+    }
+
+return (
+    <div className="date" style={{borderColor : randomColor}}>{addLinebreak(date)}</div>
+    )
+}
 
 export default Date
