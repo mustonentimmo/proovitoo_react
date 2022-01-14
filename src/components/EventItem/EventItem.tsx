a6a9e295b53f1a46a68a94967d60b9de2b95681d
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-14 09:39:14 +0200

    Refactor event item component

--- src/components/EventItem/EventItem.tsx
+++ src/components/EventItem/EventItem.tsx
@@ -3,25 +3,22 @@ import "./EventItem.scss";
 import HorizontalList from "../HorizontalList/HorizontalList";
 import Avatar from "../Avatar/Avatar";
 import Date from "../Date/Date";
+import { EventEntity } from "../../common/types";
 
 interface EventItemProps {
-    title: string;
-    eventData: string[];
-    isAvatar?: boolean;
-    avatar?: string;
-    eventDate?: string;
+    event: EventEntity;
 }
 
-const EventItem: React.FC<EventItemProps>= (props) => {
+const EventItem: React.FC<EventItemProps> = (props) => {
 
-    const { title, eventData, isAvatar, eventDate, avatar } = props;
+    const { event } = props;
 
     return (
     <div className="event-item">
-        {isAvatar ? <Avatar imgUrl={avatar} /> : <Date info={eventDate} />}
+        {event.profilePicture ? <Avatar imgUrl={event.profilePicture} /> : <Date date={event.date} />}
         <div className="event-item__info">
-            <h3 className="event-item__title">{title}</h3>
-            <HorizontalList feedback={eventData} className={"horizontal-list--small-margin-top"}/>
+            <h3 className="event-item__title">{event.name}</h3>
+            <HorizontalList feedback={event.data} className={"horizontal-list--small-margin-top"}/>
         </div>
     </div>
     )
