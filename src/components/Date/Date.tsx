420ad2d6b357d3d1c8fc3ba37548866f98c42a7f
TimmoMustonen <mustonentimmo@gmail.com>
2022-01-14 10:49:00 +0200

    Add max-size to date component and line break function

--- src/components/Date/Date.tsx
+++ src/components/Date/Date.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import "./Date.scss";
 
 interface DateProps {
-    info?: string;
+    date?: string;
 }
 
-const Date: React.FC<DateProps> = ({ info }) => (
-    <div className="date">{info}</div>
-)
+const Date: React.FC<DateProps> = ({ date }) => {
+
+    const addLinebreak = (str?: string) => {
+        return str ? str.replace(" ", "\n") : "";
+    }
+
+return (
+    <div className="date">{addLinebreak(date)}</div>
+    )
+}
 
 export default Date
