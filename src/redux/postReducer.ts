53be7054a15ecf8e0e38c4b40655cfdedfec7c81
TimmoMustonen <mustonentimmo@gmail.com>
2022-03-22 13:39:46 +0200

    Add type to initialState and default clause to switch statement

--- src/redux/postReducer.ts
+++ src/redux/postReducer.ts
@@ -1,12 +1,13 @@
 import {FETCH_POSTS_REQUEST, FETCH_POSTS_SUCCESS, FETCH_POSTS_FAILURE} from "./postTypes";
+import { InitialState, Action } from "../common/types";
 
-const initialState = {
+export const initialState: InitialState = {
     loading: false,
     posts: [],
-    error: ''
+    error: {}
 }
 
-const reducer = (state = initialState, action) => {
+export const reducer = (state = initialState, action: Action) => {
     switch (action.type) {
         case FETCH_POSTS_REQUEST:
             return {
@@ -27,7 +28,7 @@ const reducer = (state = initialState, action) => {
                 posts: [],
                 error: action.payload,
             }
+        default:
+            return state
     }
 }
-
-export default reducer;
